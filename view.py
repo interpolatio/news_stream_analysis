28fd6b1918eeaa3b104db4cdd85ebbea15155ce3
dsfd <sss@df.com>
2018-02-19 21:02:28 +0300

    correct select and moving of nodes

--- view.py
+++ view.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QToolButton, QSlider)
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QGraphicsView
+from PyQt5 import Qt
 from PyQt5 import QtGui
 from PyQt5 import QtCore
 
 class view(QGraphicsView):
+    moved = QtCore.pyqtSignal(QtGui.QMouseEvent)
     def wheelEvent(self, event):
         if (event.modifiers() == QtCore.Qt.ControlModifier):
             if (event.angleDelta().y() > 0):
@@ -15,12 +17,11 @@ class view(QGraphicsView):
             super(view, self).wheelEvent(event)
 
     def mouseMoveEvent(self, event):
-        #print("sdf")
-        #print(self.scene.selectedItems())
+
+        self.moved.emit(event)
         super(view, self).mouseMoveEvent(event)
 
     def __init__(self, owner):
-        #self.view = QGraphicsView(self)
         super(QGraphicsView, self).__init__(owner)
         self.setDragMode(QtWidgets.QGraphicsView.RubberBandDrag)
         self.setCacheMode(QtWidgets.QGraphicsView.CacheBackground)
@@ -44,22 +45,15 @@ class view(QGraphicsView):
         self.zoomSliderLayout.addWidget(self.zoomInIcon)
         self.zoomSliderLayout.addWidget(self.zoomSlider)
         self.zoomSliderLayout.addWidget(self.zoomOutIcon)
-        #QtCore.QObject.connect(self.zoomSlider, QtCore.SIGNAL('valueChanged(int)'), self.setupMatrix())
-        #self.zoomSlider.valueChanged(int)#.connect(self.setupMatrix())
         self.zoomSlider.valueChanged.connect(self.setupMatrix)
 
     def setupMatrix(self):
-        #qreal scale = qPow(qreal(2), (zoomSlider->value() - 250) / qreal(50));
-        #scale = QtCore.QMath.qPow(2.2)
-        z = QtCore.QPointF(33, 12)
+
         scale = pow(2, (self.zoomSlider.value() - 250) / 50.)
         k = QtGui.QTransform()
         k.scale(scale, scale)
         print(scale)
         self.setTransform(k)
-
-    #connect(zoomSlider, SIGNAL(valueChanged(int)), this, SLOT(setupMatrix()));
-
 
     def zoomIn(self, level: int):
         self.zoomSlider.setValue(self.zoomSlider.value() + level)
