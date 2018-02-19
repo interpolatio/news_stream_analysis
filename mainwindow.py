8e5b54661ea4ce942848609add40ba72e2106dce
dsfd <sss@df.com>
2018-02-19 21:03:15 +0300

    add VisibleWidget on/off visible nodes

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import (QDockWidget, QMainWindow, QListWidget, QTextEdit, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QSplitter, QListView)
+from PyQt5.QtWidgets import (QDockWidget, QMainWindow, QListWidget, QTextEdit, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QSplitter, QListView, QGroupBox,QCheckBox)
 from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from PyQt5.QtWidgets import QSplitter
@@ -13,11 +13,60 @@ import pandas as pd
 from node import Node
 from view import view
 
+class Communicate(QtCore.QObject):
+    closeApp = pyqtSignal()
+
+class ClassBox(QGroupBox):
+    classlists = ['asd', '123']
+    def __init__(self, owner):
+        super(ClassBox, self).__init__()
+        self.classVisibleBoxLayout = QVBoxLayout()
+        self.setTitle("Tracing Parameters")
+        owner.setWidget(self)
+        owner.setFloating(False)
+        self.classListUpdate()
+
+        #self.cur
+        # list(df.groupby('text_class')['text_class'].groups.keys())
+        #self.toggled(self.clicked)
+
+    def classListUpdate(self):
+        qChkBx_shot_all = QCheckBox("Class-" + "All", self)
+
+        qChkBx_shot_all.clicked.connect(self.toggleGroupBoxAll)
+        qChkBx_shot_all.setChecked(True)
+        self.classVisibleBoxLayout.addWidget(qChkBx_shot_all, QtCore.Qt.AlignCenter)
+        for class_group in (self.classlists):
+            qChkBx_shot = QCheckBox("Class-" + str(class_group), self)
+            qChkBx_shot.setChecked(True)
+            self.classVisibleBoxLayout.addWidget(qChkBx_shot, QtCore.Qt.AlignCenter)
+            qChkBx_shot.stateChanged.connect(self.toggleGroupBox)
+
+
+        self.setLayout(self.classVisibleBoxLayout)
+
+    def toggleGroupBoxAll(self, event):
+        check_all = self.findChildren(QCheckBox)[0]
+        flagCheckAll = check_all.isChecked()
+        for checkbox in self.findChildren(QCheckBox)[0:]:
+            checkbox.setChecked(flagCheckAll)
+
+
+    def toggleGroupBox   (self, event):
+        flagCheck = True
+        for checkbox in self.findChildren(QCheckBox)[1:]:
+            if not checkbox.isChecked():
+                flagCheck = False
+        self.findChildren(QCheckBox)[0].setChecked(flagCheck)
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent = None):
         super(MainWindow, self).__init__(parent)
         self.view = view(self)
         self.timerId = 0
+        self.c = Communicate()
+
 
         df = process.get_dataframe()
 
@@ -33,21 +82,23 @@ class MainWindow(QMainWindow):
         file.addAction("quit")
 
         self.items = QDockWidget("Title file", self)
-        self.textWidget = QListView()
-        #self.listWidget = QListWidget()
-        #self.listWidget.addItem("item1")
-        #self.listWidget.addItem("item2")
-        #self.listWidget.addItem("item3")
+        self.classVisible = QDockWidget("Class Visible", self)
 
+        self.textWidget = QListView()
         self.items.setWidget(self.textWidget)
         self.items.setFloating(False)
+
+
         self.setCentralWidget(self.view)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.items)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.classVisible)
+
         self.setLayout(layout)
         self.setWindowTitle("Text Visualization")
 
-        self.view.scene.mouseMoveEvent = lambda event: self.ListViewUpdate()
+        self.view.moved.connect(self.ListViewUpdate)
 
+        self.Class = ClassBox(self.classVisible)
 
     def ListViewUpdate(self):
         model = QtGui.QStandardItemModel()
@@ -55,3 +106,11 @@ class MainWindow(QMainWindow):
             item = QtGui.QStandardItem(node.title)
             model.appendRow(item)
         self.textWidget.setModel(model)
+
+    def classVisibleUpdate(self):
+        model = QtGui.QStandardItemModel()
+        for node in self.view.scene.selectedItems():
+            item = QtGui.QStandardItem(node.title)
+            model.appendRow(item)
+        self.textWidget.setModel(model)
+
